eb6e0ed5aab11726f3e72a3e91308e866a638f40
asalihaj <as48243@ubt-uni.net>
2021-05-27 23:34:08 +0200

    End of Section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,13 +7,15 @@ interface IProps {
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 }
 
 export const ActivityForm: React.FC<IProps> = ({
     setEditMode, 
     activity: initialFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
 
     const initializeForm = () => {
@@ -57,10 +59,10 @@ export const ActivityForm: React.FC<IProps> = ({
                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title}/> 
                <Form.TextArea onChange={handleInputChange} name='description' rows={2} placeholder='Description' value={activity.description}/> 
                <Form.Input onChange={handleInputChange} name='category' placeholder='Category'value={activity.category}/> 
-               <Form.Input onChange={handleInputChange} name='datetime-local' type='date' placeholder='Date'value={activity.date}/> 
+               <Form.Input onChange={handleInputChange} name='date' type='datetime-local' placeholder='Date'value={activity.date}/> 
                <Form.Input onChange={handleInputChange} name='city' placeholder='City'value={activity.city}/> 
                <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue'value={activity.venue}/> 
-               <Button floated='right' positive type='submit' content='Submit' />
+               <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                <Button onClick={() => setEditMode(false)} floated='right' type='submit' content='Cancel' />
             </Form>
         </Segment>
