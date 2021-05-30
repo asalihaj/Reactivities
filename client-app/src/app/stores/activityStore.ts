712dd2dd79f97cf5746758d739cda6fe321847e6
asalihaj <as48243@ubt-uni.net>
2021-05-30 18:21:00 +0200

    End of Section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -6,21 +6,19 @@ import agent from '../api/agent';
 configure({enforceActions: 'always'});
 
 class ActivityStore {
-  activityRegistry = new Map();
-  activities: IActivity[] = [];
-  selectedActivity: IActivity | undefined;
-  loadingInitial = false;
-  editMode = false;
-  submitting = false;
-  target = '';
+  @observable activityRegistry = new Map();
+  @observable activity: IActivity | null = null;
+  @observable loadingInitial = false;
+  @observable submitting = false;
+  @observable target = '';
 
-  get activitiesByDate() {
+  @computed get activitiesByDate() {
     return Array.from(this.activityRegistry.values()).sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
   }
 
-  loadActivities = async () => {
+  @action loadActivities = async () => {
     this.loadingInitial = true;
     try {
       const activities = await agent.Activities.list();
@@ -39,13 +37,42 @@ class ActivityStore {
     }
   };
 
-  createActivity = async (activity: IActivity) => {
+  @action loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if (activity) {
+      this.activity = activity;
+    } else {
+      this.loadingInitial = true;
+      try {
+        activity = await agent.Activities.details(id);
+        runInAction(() => {
+          this.activity = activity;
+          this.loadingInitial = false;
+        });
+      } catch (e) {
+        runInAction(() => {
+          this.loadingInitial = false;
+          
+        });
+        console.log(e);
+      }
+    }
+  }
+
+  @action clearActivity = () => {
+    this.activity = null;
+  }
+
+  getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+  }
+
+  @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
       runInAction(() => {
         this.activityRegistry.set(activity.id, activity);
-        this.editMode = false;
         this.submitting = false;
       })
     } catch (error) {
@@ -56,14 +83,13 @@ class ActivityStore {
     }
   };
 
-  editActivity = async (activity: IActivity) => {
+  @action editActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
       await agent.Activities.update(activity);
       runInAction(() => {
         this.activityRegistry.set(activity.id, activity);
-        this.selectedActivity = activity;
-        this.editMode = false;
+        this.activity = activity;
         this.submitting = false;
       })
 
@@ -75,7 +101,7 @@ class ActivityStore {
     }
   };
 
-  deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+  @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
     this.submitting = true;
     this.target = event.currentTarget.name;
     try {
@@ -92,49 +118,8 @@ class ActivityStore {
       })
       console.log(error);
     }
-  }
-
-  openCreateForm = () => {
-    this.editMode = true;
-    this.selectedActivity = undefined;
   };
 
-  openEditForm = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
-    this.editMode = true;
-  }
-
-  cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
-  }
-
-  cancelFormOpen = () => {
-    this.editMode = false;
-  }
-
-  selectActivity = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
-    this.editMode = false;
-  };
 }
-
-decorate(ActivityStore, {
-  activityRegistry: observable,
-  activities: observable,
-  selectedActivity: observable,
-  loadingInitial: observable,
-  editMode: observable,
-  submitting: observable,
-  target: observable,
-  activitiesByDate: computed,
-  loadActivities: action,
-  createActivity: action,
-  editActivity: action,
-  deleteActivity: action,
-  openCreateForm: action,
-  cancelSelectedActivity: action,
-  cancelFormOpen: action,
-  selectActivity: action,
-})
 
 export default createContext(new ActivityStore());
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './app/layout/style.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
